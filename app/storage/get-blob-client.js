2cf62d7158e4439683000577bad9b5b2ca667f2b
Shaun Fitzsimons <87677173+sfitz42@users.noreply.github.com>
2024-06-07 14:50:00 +0100

    Update SND3 config (#20)
    * Update programmeName
    
    * Update SND3 config
    
    * Bump package version
    
    * Update events topic
    
    * Add cluster ip template
    
    * Bump package version
    
    * Add missing storage helm template
    
    * Fix token invalidation

--- app/storage/get-blob-client.js
+++ app/storage/get-blob-client.js
@@ -2,12 +2,8 @@ const { BlobServiceClient } = require('@azure/storage-blob')
 const { DefaultAzureCredential } = require('@azure/identity')
 const config = require('../config/storage')
 
-let blobServiceClient
-
 const getBlobClient = () => {
-  if (blobServiceClient) {
-    return blobServiceClient
-  }
+  let blobServiceClient
 
   if (config.useConnectionString) {
     console.log('Using connection string for BlobServiceClient')
@@ -15,7 +11,7 @@ const getBlobClient = () => {
   } else {
     console.log('Using DefaultAzureCredential for BlobServiceClient')
     const uri = `https://${config.storageAccount}.blob.core.windows.net`
-    blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential())
+    blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential({ managedIdentityClientId: process.env.AZURE_CLIENT_ID }))
   }
 
   return blobServiceClient
